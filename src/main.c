d8ec9fc75fe7eedc5b9097736e5c643d746625f9
Hien Nguyen <vanhien.nguyen@tum.de>
2020-11-18 14:11:16 +0100

    challenge II

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-int main() {
-	printf("Hello ESPL \n");
+
+#include "espl_lib.h"
+
+int main() 
+{
+	char ch;
+	while (1){
+		unsigned int i;  
+        printf("Please enter a number: ");
+        scanf("%d", &i);
+        printf("The number in string: %s\n",num_to_words(i));
+        printf("Enter any key to continue or 'q' to exit: ");
+		scanf(" %c", &ch);
+        if(ch=='Q' || ch=='q'){
+            break;
+        }
+	}
 	return 0;
 }
